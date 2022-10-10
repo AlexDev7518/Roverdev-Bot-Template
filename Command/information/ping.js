abd1842107b40551e78cf7f353f49784c0e8b8f7
AlexDev7518 <83729002+AlexDev7518@users.noreply.github.com>
2022-10-10 06:47:22 -0700

    Update ping.js

--- Command/information/ping.js
+++ Command/information/ping.js
@@ -1,25 +1,8 @@
 const { EmbedBuilder, ApplicationCommandType, ApplicationCommandOptionType } = require("discord.js")
 
 module.exports.prefixRun = async (client, message, args, cmdUser,prefix) => {
-    const msgId = await message.reply('🏓 Pong!')
-    const embed = new EmbedBuilder()
-    .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
-    .setColor('Random')
-    .setTimestamp()
-  .setColor("#00FFFF")
-    .setDescription(`**Time:** ${Math.floor(msgId.createdTimestamp - message.createdTimestamp)} ms\n**API Ping:** ${client.ws.ping} ms`)
-    msgId.edit({ embeds: [embed], content: `<@${message.author.id}>` })
 }
 module.exports.slashRun = async (interaction, client) => {
-    await interaction.reply('🏓 Pong!')
-    const msg = await interaction.fetchReply()
-    const embed = new EmbedBuilder()
-    .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
-    .setColor('Random')
-    .setTimestamp()
-  .setColor("#00FFFF")
-    .setDescription(`**Time:** ${Math.floor(msg.createdTimestamp - interaction.createdTimestamp)} ms\n**API Ping:** ${client.ws.ping} ms`)
-    interaction.editReply({ embeds: [embed], content: `<@${interaction.user.id}>` })
 }
 
 module.exports.conf = {
